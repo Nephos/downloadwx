4f7fc8bcac0491fddd893c313f51df2ca4163b90
Arthur Poulet <arthur.poulet@mailoo.org>
2016-10-20 12:04:54 +0100

    Improve status_code on error

--- src/downloadwx/www/pause.cr
+++ src/downloadwx/www/pause.cr
@@ -1,6 +1,11 @@
 get "/pause/:id" do |env|
   env.response.content_type = "application/json"
   id = env.params.url["id"].to_i
-  D.pause(id)
-  D[id].to_h
+  begin
+    D.pause(id)
+    D[id].to_h
+  rescue e
+    env.response.status_code = 500
+    {"message" => e.message}
+  end
 end
--- src/downloadwx/www/resume.cr
+++ src/downloadwx/www/resume.cr
@@ -1,6 +1,11 @@
 get "/resume/:id" do |env|
   env.response.content_type = "application/json"
   id = env.params.url["id"].to_i
-  D.resume env.params.url["id"].to_i
-  D[id].to_h
+  begin
+    D.resume(id)
+    D[id].to_h
+  rescue e
+    env.response.status_code = 500
+    {"message" => e.message}
+  end
 end
